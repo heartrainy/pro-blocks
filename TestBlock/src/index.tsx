0ab16a0a89fc3fa233090a9e6a88d952316c4994
heartrainy <526132448@qq.com>
2019-07-04 15:10:52 +0800

    设置宏

--- TestBlock/src/index.tsx
+++ TestBlock/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './index.css';
 
-const Block: React.FC = () => {
+const PAGE_NAME_UPPER_CAMEL_CASE = () => {
   return (
     <div className={styles.normal}>
-      <h1>I am a umi block!</h1>
+      <h1>I am a umi block!3333</h1>
     </div>
   );
 };
 
-export default Block;
+export default PAGE_NAME_UPPER_CAMEL_CASE;
